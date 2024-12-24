7f0ca843f549a597a98e8fc848a9f004e6b0c910
narestakian <n.arestakian@gmail.com>
2024-12-24 20:25:38 +0400

    added changes in pom.xml

--- src/main/java/StepsSelenide/WebTableTestSteps.java
+++ src/main/java/StepsSelenide/WebTableTestSteps.java
@@ -5,11 +5,8 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class WebTableTestSteps extends WebTableTestElements {
 
-
     public void openTablesPage () {
-
         open("https://the-internet.herokuapp.com/tables");
-
     }
 
     public void printTableData() {
